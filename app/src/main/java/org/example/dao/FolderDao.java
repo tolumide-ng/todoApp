f39753c516b879cd541da3de2ec9df20dea7876a
Tolumide Shopein <tolumide.shopein@hivemq.com>
2024-08-13 16:58:53 +0200

    Fetches and properly labells the meta and children of the root folder

--- app/src/main/java/org/example/dao/FolderDao.java
+++ app/src/main/java/org/example/dao/FolderDao.java
@@ -12,7 +12,9 @@ import org.jdbi.v3.sqlobject.statement.SqlQuery;
 import org.jdbi.v3.sqlobject.statement.SqlUpdate;
 
 public interface FolderDao {
-    @SqlQuery("SELECT f.id AS id, f.name AS name, " +
+    @SqlQuery("SELECT " +
+            "CASE WHEN :id::UUID IS NULL THEN NULL ELSE f.id END AS id, " +
+            "CASE WHEN :id::UUID IS NULL THEN '' ELSE f.name END AS name, " +
             "ARRAY_AGG(DISTINCT ROW(child_folder.name, child_folder.id)) AS folders, " +
             "CASE WHEN COUNT(child_file.id) > 0 THEN ARRAY_AGG(DISTINCT ROW(child_file.name, child_file.id)) ELSE ARRAY[]::record[] END AS files "
             +
@@ -20,9 +22,14 @@ public interface FolderDao {
             "LEFT JOIN task child_file ON (:id::UUID IS NULL AND child_file.parent IS NULL) OR (child_file.parent = :id::UUID) "
             +
             "LEFT JOIN folder child_folder ON (:id::UUID IS NULL AND child_folder.parent IS NULL) OR (child_folder.parent = :id::UUID) "
-            +
-            "WHERE (:id::UUID IS NULL AND f.parent IS NULL) OR (:id::UUID IS NOT NULL AND f.id = :id::UUID) " +
+            // "LEFT JOIN task child_file ON (child_file.parent = COALESCE(:id::UUID,
+            // NULL))"
+            // + "LEFT JOIN folder child_folder ON (child_folder.parent =
+            // COALESCE(:id::UUID, NULL))"
+            + "WHERE (:id::UUID IS NULL AND f.parent IS NULL) OR (f.id = :id::UUID)" +
+            // "(:id::UUID IS NOT NULL AND f.id = :id::UUID) " +
             "GROUP BY f.id, f.name;")
+
     @RegisterRowMapper(FolderMapper.class)
     Folder getOneFolder(@Bind("id") UUID id);
 
