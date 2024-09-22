d07d3acb52c012546ea8f8ef7cbf7cc440f039cf
Tolumide Shopein <tolumideshopein@gmail.com>
2024-09-22 15:32:12 +0200

    Removes app/bin from tracked files

--- app/bin/main/db/migration/V1.0.1__Create_folder_table.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE folder (
-    id UUID PRIMARY KEY UNIQUE DEFAULT uuid_generate_v4(),
-    parent UUID,
-    name TEXT,
-    owner UUID NOT NULL,
-    updated_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
-    created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    UNIQUE(name, parent)
-)
